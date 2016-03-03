d12656de203000255a6ce504f7ae04191559fbb8
Roy Willemse <roy.willemse@crv4all.com>
2016-03-03 18:07:54 +0100

    Grails 3.1.3 applications; 'service' (bearer) and 'client' (authorization code)

---
+++ client/grails-app/controllers/client/ExampleController.groovy
@@ -0,0 +1,14 @@
+package client
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate
+
+class ExampleController {
+
+    KeycloakRestTemplate keycloakRestTemplate
+    
+    def index() {
+        render contentType: 'application/json', encoding: 'UTF-8', 
+            text: keycloakRestTemplate.getForObject("http://localhost:8081/service/principal", JsonNode.class)
+    }
+}
---
+++ service/grails-app/init/service/Application.groovy
@@ -0,0 +1,12 @@
+package service
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan(basePackages = "service")
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
