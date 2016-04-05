1886aa60f554aeefd30fd9e4a9083ec745eff763
Roy Willemse <roy.willemse@crv4all.com>
2016-04-05 17:36:12 +0200

    Add example for loading different keycloak.json based on System property.
    Note that keycloak.json moved from src/main/webapp/WEB-INF/ to
    src/main/resources/

--- resource/src/main/java/resource/Application.java
+++ resource/src/main/java/resource/Application.java
@@ -1,5 +1,9 @@
 package resource;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +18,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.io.InputStream;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -35,6 +41,41 @@ public class Application extends SpringBootServletInitializer {
         @Autowired
         public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
             auth.authenticationProvider(keycloakAuthenticationProvider());
+        }
+
+        /**
+         * Overrides default keycloak config resolver behaviour (/WEB-INF/keycloak.json) by a simple mechanism.
+         * <p>
+         * This example loads other-keycloak.json when the parameter use.other is set to true, e.g.:
+         * {@code ./gradlew bootRun -Duse.other=true}
+         *
+         * @return keycloak config resolver
+         */
+        @Bean
+        public KeycloakConfigResolver keycloakConfigResolver() {
+            return new KeycloakConfigResolver() {
+
+                private KeycloakDeployment keycloakDeployment;
+
+                @Override
+                public KeycloakDeployment resolve(HttpFacade.Request facade) {
+                    if (keycloakDeployment != null) {
+                        return keycloakDeployment;
+                    }
+
+                    boolean useOther = System.getProperty("use.other", "false").equals("true");
+                    String path = useOther ? "/other-keycloak.json" : "/keycloak.json";
+                    InputStream configInputStream = getClass().getResourceAsStream(path);
+
+                    if (configInputStream == null) {
+                        throw new RuntimeException("Could not load Keycloak deployment info: " + path);
+                    } else {
+                        keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+                    }
+
+                    return keycloakDeployment;
+                }
+            };
         }
 
         @Bean
